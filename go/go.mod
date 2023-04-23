796b0df5769b1abbce7f8d94606d996c4312e368
Ian Ross <ian@skybluetrades.net>
2023-04-23 14:15:43 +0200

    Add Go example

---
+++ go/go.mod
@@ -0,0 +1,5 @@
+module github.com/dotenv-org/godotenvvault-example
+
+go 1.20
+
+require github.com/dotenv-org/godotenvvault v0.3.0
--- go/index.go
+++ go/index.go
@@ -1,28 +1,96 @@
 package main
 
-import "log"
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"encoding/base64"
+	"encoding/hex"
+	"errors"
+	"fmt"
+	"log"
+)
 
-// implement
-func encrypt(key string, message string) {
-  log.Println(key)
-  log.Println(message)
-}
-
-// implement
-func decrypt(key string, ciphertext string) {
-  log.Println(key)
-  log.Println(ciphertext)
-}
+const nonceBytes = 12
 
 func main() {
-  key := "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" 
-  message := "HELLO"
+	key := "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+	message := "HELLO"
 
-  encrypt(key, message)
-  decrypt(key, "ciphertext")
-  // ciphertext = EnvVaultExample.new.encrypt(key, message)
-  //puts ciphertext
+	cipherText, err := encrypt(key, message)
+	if err != nil {
+		log.Fatal("error in encrypt: ", err)
+	}
+	fmt.Println("Cipher text:", cipherText)
 
-  // decryptedtext = EnvVaultExample.new.decrypt(key, ciphertext)
-  //puts decryptedtext
+	plainTextBytes, err := decrypt(key, cipherText)
+	if err != nil {
+		log.Fatal("error in decrypt: ", err)
+	}
+	plainText := string(plainTextBytes)
+	fmt.Println("Plain text:", plainText)
+
+	if plainText != message {
+		fmt.Println("ERROR: plaintext doesn't match original message")
+	}
+}
+
+// implement
+func encrypt(key string, plainText string) (string, error) {
+	if len(key) < 64 {
+		return "", errors.New("INVALID_DOTENV_KEY: Key part must be 64 characters long (or more)")
+	}
+
+	// Set up key.
+	keyBytes, _ := hex.DecodeString(key)
+
+	// Generate random nonce.
+	nonceBytes := generateNonce()
+
+	// Set up cipher.
+	block, _ := aes.NewCipher(keyBytes)
+	aesgcm, _ := cipher.NewGCM(block)
+
+	// Encrypt.
+	cipherTextBytes := aesgcm.Seal(nil, nonceBytes, []byte(plainText), nil)
+
+	// Prepend nonce and base64 encode.
+	messageBytes := append(nonceBytes, cipherTextBytes...)
+	return base64.StdEncoding.EncodeToString(messageBytes), nil
+}
+
+// Decrypt a single encrypted environment string using the supplied
+// key. The cipher is AES-GCM, and the first 12 bytes of the
+// ciphertext are used as the nonce value.
+func decrypt(key string, cipherText string) ([]byte, error) {
+	if len(key) < 64 {
+		return nil, errors.New("INVALID_DOTENV_KEY: Key part must be 64 characters long (or more)")
+	}
+
+	// Set up key.
+	keyBytes, _ := hex.DecodeString(key)
+
+	// Base64 decode input.
+	cipherTextBytes, _ := base64.StdEncoding.DecodeString(cipherText)
+
+	// Extract nonce.
+	nonceBytes := cipherTextBytes[:12]
+
+	// Extract cipher text.
+	cipherTextBytes = cipherTextBytes[12:]
+
+	// Set up cipher.
+	block, _ := aes.NewCipher(keyBytes)
+	aesgcm, _ := cipher.NewGCM(block)
+
+	// Decrypt.
+	plainText, _ := aesgcm.Open(nil, nonceBytes, cipherTextBytes, nil)
+
+	return plainText, nil
+}
+
+func generateNonce() []byte {
+	nonce := make([]byte, nonceBytes)
+	_, _ = rand.Read(nonce)
+	return nonce
 }
